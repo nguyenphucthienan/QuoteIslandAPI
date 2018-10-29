a40bfed79b4f3e4e36ff3f76e4fccb31bc8731dc
Nguyen Phuc Thien An <npta97@gmail.com>
2018-10-29 11:10:47 +0700

    Add seed for categories

--- seeds/Seeds.js
+++ seeds/Seeds.js
@@ -1,11 +1,15 @@
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const Role = mongoose.model('Role');
+const Category = mongoose.model('Category');
+
 const RoleNames = require('../constants/RoleNames');
+const categoriesData = require('./categories.json');
 
 function seedRoles() {
   _.each(RoleNames, async (roleName) => {
     const role = await Role.findOne({ name: roleName });
+
     if (!role) {
       const newRole = new Role({ name: roleName });
       await newRole.save();
@@ -13,4 +17,18 @@ function seedRoles() {
   });
 }
 
+function seedCategories() {
+  categoriesData.forEach(async (categoryData) => {
+    const category = await Category.findOne({
+      name: categoryData.name
+    });
+
+    if (!category) {
+      const newCategory = new Category({ ...categoryData });
+      await newCategory.save();
+    }
+  });
+}
+
 seedRoles();
+seedCategories();
